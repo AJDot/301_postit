cbf124bd439fc6ceb8149cbadd711846cdb54706
Alex Henegar <alex.henegar19@gmail.com>
2017-12-19 09:44:40 -0500

    complete course 3 - prep for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,35 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] # memoization
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:error] = "Must be logged in to do that."
+      redirect_to root_path
+    end
+  end
+
+  def require_no_user
+    if logged_in?
+      flash[:error] = "Can't be logged in to do that."
+      redirect_to root_path
+    end
+  end
+
+  def require_owner(user)
+    if current_user != user
+      flash[:error] = "Must be the owner to do that."
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,41 @@
 class CommentsController < ApplicationController
-  def create
-    @post = Post.find(params[:post_id])
-    # @comment = Comment.new(params.require(:comment).permit(:body))
-    # @comment.post = @post
-    @comment = @post.comments.build(params.require(:comment).permit(:body))
-    @comment.creator = User.first # TODO: fix after authentication
+  before_action :set_post, only: [:create, :vote]
+  before_action :set_comment, only: [:vote]
+  before_action :require_user
 
-    if @comment.save
+  def create
+    @comment = Comment.new(params.require(:comment).permit(:body))
+
+    if @comment.valid?
+      @comment.creator = current_user
+      @comment.post = @post
+      @comment.save
       flash[:notice] = "Your comment was added."
       redirect_to post_path(@post)
     else
       render 'posts/show'
     end
+  end
+
+  def vote
+    @vote = Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
+
+    if @vote.valid?
+      flash[:notice] = "Your vote was counted."
+    else
+      flash[:error] = "You can only vote on a comment once."
+    end
+
+    redirect_to :back
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
 end
